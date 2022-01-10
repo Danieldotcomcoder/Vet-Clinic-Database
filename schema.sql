24da5f037647812b8a417e74135e7505a2a9d8f5
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-01-10 17:12:58 +0200

    Add: NOT NULL

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 CREATE DATABASE vet_clinic;
 CREATE TABLE animals (
-    id                INT,
+    id      INT PRIMARY KEY  NOT NULL,,
     name              varchar(50),
     date_of_birth     date,
     escape_attempts   INT,

ba2815e27defa190f78d98d793c6d2d803ea5318
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-01-11 10:47:51 +0200

    Fix: Table

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 CREATE DATABASE vet_clinic;
 CREATE TABLE animals (
-    id      INT PRIMARY KEY  NOT NULL,,
+    id      INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
     name              varchar(50),
     date_of_birth     date,
     escape_attempts   INT,
     neutered          boolean,
     weight_Kg         decimal
 );
+
